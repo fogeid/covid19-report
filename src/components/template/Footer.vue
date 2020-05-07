021ea5eff7e9382bfc360eb9a60c21b30b4a55db
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2020-05-07 00:12:31 -0400

    update

--- src/components/template/Footer.vue
+++ src/components/template/Footer.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
         <div class="footer">
-            
+            <p class="footer__title">2020 © Developed by <a class="footer__title__link" :href="github">Diego Batista</a></p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            github: 'https://github.com/fogeid',
+        }
+    },
 }
 </script>
 
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,19 +1,19 @@
 <template>
     <div id="app">
-        <div>
-            <header>
-                <app-header />                
-            </header>
-        </div>
-        <div class="container-fluid">
+        <header class="header">
+            <app-header />
+        </header>
+        <main class="container-fluid main">
             <div>
                 <app-select />
             </div>
             <div>
                 <!-- <app-card /> -->
             </div>
-        </div>
-        <!-- <app-footer /> -->
+        </main>
+        <footer class="footer">
+            <app-footer />
+        </footer>
     </div>
 </template>
 
@@ -22,7 +22,7 @@
 import Header from '@/components/template/Header.vue';
 import Select from '@/components/template/Select.vue';
 // import Card from '@/components/template/Card.vue';
-// import Footer from '@/components/template/Footer.vue';
+import Footer from '@/components/template/Footer.vue';
 
 export default {
     name: 'Home',
@@ -30,7 +30,7 @@ export default {
         'app-header': Header,
         'app-select': Select,
         // 'app-card': Card,
-        // 'app-footer': Footer
+        'app-footer': Footer
     }
 }
 </script>
